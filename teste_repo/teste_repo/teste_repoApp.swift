c2bb25a80e790da286af72ea0fb44d5926ccfcbe
Erilane Teixeira <erilanepereiradcs@gmail.com>
2023-04-26 09:09:44 -0300

    Criar o projeto inicial

---
+++ teste_repo/teste_repo/teste_repoApp.swift
@@ -0,0 +1,17 @@
+//
+//  teste_repoApp.swift
+//  teste_repo
+//
+//  Created by Erilane Teixeira on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct teste_repoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
