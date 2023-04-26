b697c26e8977ef30f6d88a2198bb128377d6e4d6
Erilane Teixeira <erilanepereiradcs@gmail.com>
2023-04-26 09:40:16 -0300

    Alterar texto e ícone da Content View

--- teste_repo/teste_repo/ContentView.swift
+++ teste_repo/teste_repo/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, team!!")
         }
         .padding()
     }
